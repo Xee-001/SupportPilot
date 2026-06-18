56239be8d67601841d674a7f849fad5a6d602f77
Akansh Jaiswal <akanshjaiswal321@gmail.com>
2026-06-19 00:06:55 +0530

    "fix2"

--- query.py
+++ query.py
@@ -1,3 +1,4 @@
+from rank_bm25 import BM25Okapi
 from dotenv import load_dotenv
 import chromadb
 from groq import Groq
@@ -10,14 +11,39 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 client = chromadb.PersistentClient(path="./chroma_db")
 collection = client.get_or_create_collection("fastapi_docs")
 
+all_docs= collection.get()  #fetches all documents in the collection(chroma)
+corpus = all_docs["documents"] # list of all the text chunks in the collection
+ids = all_docs["ids"]
+
+tokenized_corpus = [doc.split() for doc in corpus]
+bm25 = BM25Okapi(tokenized_corpus)
+
+
 def answer_question(question):
     embedding = model.encode([question]).tolist()
     results = collection.query(query_embeddings=embedding, n_results=3)
     
-    chunks = results["documents"][0]
-    context = "\n\n".join(chunks)
+    tokenized_query = question.split()
+    bm25_scores = bm25.get_scores(tokenized_query)
+    bm25_top_indices = sorted(range(len(bm25_scores)), key=lambda i: bm25_scores[i], reverse=True)[:3]
+    bm25_top_chunks = [corpus[i] for i in bm25_top_indices]
+    bm25_ids = [ids[i] for i in bm25_top_indices]
 
-    sources = [m["source"] for m in results["metadatas"][0]]
+    vector_ids = results["ids"][0]
+    rrf_scores = {}
+    for rank, doc_id in enumerate(vector_ids):
+        rrf_scores[doc_id] = rrf_scores.get(doc_id, 0) + 1/(rank + 60)
+    for rank, doc_id in enumerate(bm25_ids):
+        rrf_scores[doc_id] = rrf_scores.get(doc_id, 0) + 1/(rank + 60)
+    top_ids = sorted(rrf_scores, key=rrf_scores.get, reverse=True)[:3]
+
+    id_to_chunk = dict(zip(ids, corpus))
+    final_chunks = [id_to_chunk[i] for i in top_ids]
+    context = "\n\n".join(final_chunks)
+
+
+    sources = list(set([id.rsplit("_", 1)[0] for id in top_ids]))
+
 
     prompt = f"""You are a helpful assistant that answers questions based on the following context below.
     
@@ -37,6 +63,8 @@ def answer_question(question):
     )
     answer = response.choices[0].message.content
     return {"answer": answer, "sources": sources}
+
+
 
 if __name__ == "__main__":
     result = answer_question("What is FastAPI?")
